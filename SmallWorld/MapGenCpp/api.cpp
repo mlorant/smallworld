14ce182ae47b0a48482057215182fdfc20108644
Maxime Lorant <maxime.lorant@gmail.com>
2013-11-27 22:58:27 +0100

    doc CppLib and add ideas to implement

--- SmallWorld/MapGenCpp/api.cpp
+++ SmallWorld/MapGenCpp/api.cpp
@@ -1,6 +1,15 @@
 #include "api.h"
 #include "map.h"
 
+/*!
+ * \brief Generate a map with random cases
+ *
+ * Generate an array of numbers, which represents squares types.
+ * Representation of numbers are given by the `CaseType` enum.
+ *
+ * \param size : number of squares of the map (side length)
+ * \return array of number between 0 and 4, with a length of size^2
+ */
 int* api_generate_map(int size) {
 	MapGenerator gen;
 	return gen.generate_map(size);
--- SmallWorld/MapGenCpp/map.cpp
+++ SmallWorld/MapGenCpp/map.cpp
@@ -1,16 +1,34 @@
 #include "map.h"
 
+enum CaseType
+{
+	Desert = 0,
+	Forest,
+	Mountain,
+	Plain,
+	Sea
+};
+
+/*!
+ * \brief Generate a map with random cases
+ *
+ * Generate an array of numbers, which represents squares types.
+ * Representation of numbers are given by the `CaseType` enum.
+ *
+ * \param size : number of squares of the map (side length)
+ * \return array of number between 0 and 4, with a length of size^2
+ */
 int* MapGenerator::generate_map(int size) {
 	
-	// Init randomness
+	// Init random seed and map array
 	srand (time(NULL));
-
 	int* map = (int*) malloc(size * size * sizeof(int*));
 	
-	int i, j;
+	int i;
+	// TODO: Implement map generation algorithm. Idea : Perlin Noise
 	for(i = 0; i < size * size; i++) {
-			map[i] = rand() % 5;
+		map[i] = rand() % 5;
 	}
-
+	
 	return map;
 }
